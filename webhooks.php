a3c22b7e57edc422e095241e2397523638bf16de
sittikorn2537 <42830288+sittikorn2537@users.noreply.github.com>
2019-05-14 13:30:25 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $event['message']
+				'text' => '55555'
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
